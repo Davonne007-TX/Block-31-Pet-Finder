b7e9b5dcbcfa0ce5e94e88b0f0d2600884a489d8
Vonney <davonnejv@gmail.com>
2023-08-10 19:57:11 -0500

    tidying up code

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // import the pets array from data.js
+  //you can also require 
 const pets = require('./data');
 
 // init express app
 const express = require('express');
 const app = express();
 
-const PORT = 3000;
+const PORT = 3000; //changed the port to 3000, 8080 was not loading 
 
 //middleware
 app.use(express.static('public'))
@@ -29,33 +30,31 @@ app.get('/api/v1/pets', (req, res) => {
    res.send(pets);
 });
 
-// get pet by owner with query string         //is this right ???     
+// get pet by owner with query string      
     //set deconstructed owner to req.query
-    //send the response 
+    //console log the owner 
+    //send the response to pet
     app.get('/api/v1/pets/owner', (req, res) => {
         const {owner} = req.query
-        console.log(`Getting the pets owner, ${owner}`) //this is undefined ...? 
+        console.log(`Getting the pets owner, ${owner}`) 
     
         // find the pet in the pets array
         const pet = pets.find(pet => pet.owner === owner);
     
         // send the pet as a response
-        res.send(pets)
+        res.send(pet)
     });
 
 // get pet by name
     //deconstruct name
     //request params
-    //respond send the deconstructed
-
-//??? Am i supposed to show the data ??? 
+    //respond send, pet
 app.get('/api/v1/pets/:name', (req, res) => {
     const {name} = req.params
-    res.send(`Getting pet by the name ${name}`)
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
-
+    res.send(pet)
 });
 
 app.listen(PORT, () => {
@@ -63,3 +62,7 @@ app.listen(PORT, () => {
 });
 
 module.exports = app;
+
+
+
+
